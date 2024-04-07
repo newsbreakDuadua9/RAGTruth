ebf7bc2b4b8dbd4c245bff8f6a7010611dcfe000
siliang <93968213+CodingLL@users.noreply.github.com>
2024-04-07 20:59:08 +0000

    upload lora weights organize file structure

--- step1_lora_merge.sh
+++ step1_lora_merge.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "start merging lora weights!"
 
-CUDA_VISIBLE_DEVICES=1 \
 python models/merge_lora_weights.py \
 --base_model meta-llama/Llama-2-13b-hf \
---peft_model models/model_file/llama2_13b_lora_0217_ckpt \
---output_dir models/model_file/llama2_13b_lora_0217_ckpt_merge \
+--peft_model models/model_file/ckpt/llama2_13b_lora_0217 \
+--output_dir models/model_file/merged/llama2_13b_lora_0217_merge \
 --safe_tensor True
 
 echo "merge done!"
-
